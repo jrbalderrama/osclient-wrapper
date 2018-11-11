2fe8404680f6193a2aab1b734492cab976f2869e
jrbalderrama <jrbalderrama@users.noreply.github.com>
2018-11-11 15:21:28 +0100

    adrien modifications

--- mitmdump_script.py
+++ mitmdump_script.py
@@ -1,12 +1,13 @@
-from mitmproxy import http
 from mitmproxy import ctx
-
-
 def request(flow: http.HTTPFlow) -> None:
     #print ("X-Auth-Token %s", flow.request.headers[X-Auth-Token])
-    ctx.log.info("We've seen %s X-Auth-Token" % flow.request.headers['X-Auth-Token'])
-    flow.request.headers['X-Auth-Token'] = str(flow.request.headers['X-Auth-Token']) + "!SCOPE!{'Nova':'R1','Keystone':'R2'}"
-    flow.request.headers.update(scope = "{'Nova':'R1','Keystone':'R2'}")
-
+    #try:
+    # ctx.log.info("We've seen %s X-Auth-Token" % flow.request.headers['X-Auth-Token'])
+    try:
+        flow.request.headers['X-Auth-Token'] = str(flow.request.headers.get('X-Auth-Token')) + "!SCOPE!{'Nova':'R1','Keystone':'R2'}"
+    except Exception:
+        pass
+    #except Exception as e:
+    #   print(str(e))    flow.request.headers.update(scope = "{'Nova':'R1','Keystone':'R2'}")
 
 # OS_REGION_NAME
--- wrapper.py
+++ wrapper.py
@@ -1,13 +1,14 @@
+#!/usr/bin/python3
+
 import logging
 import os
 import random
 import signal
 import sys
 import subprocess
-
+import time
 
 logger = logging.getLogger(__name__)
-
 
 def _configure_logging(args):
     if '-vv' in args:
@@ -23,7 +24,6 @@ def _configure_logging(args):
         logging.basicConfig(level=logging.INFO)
     return args
 
-
 def _get_scope_et_all(args):
     scope = None
     if '--scope' in args:
@@ -37,34 +37,37 @@ def _run_openstack(port, args):
     url = "http_proxy='http://localhost:{}'".format(port)
 
     # http_proxy='http://localhost:YOUR_RANDOM_PORT" OPENSTACK_REQUEST
-    proxy_prefixed_command = [url] + args
+    proxy_prefixed_command = url + " openstack " + " ".join(args)
+    #print (proxy_prefixed_command)
     stdout = subprocess.run(proxy_prefixed_command,
                             shell=True, check=True, stdout=True)
-    logger.debug(stdout)
+    #print(stdout.stdout)
+    #logger.debug(stdout)
 
-import psutil
 
 def _run_proxy_server(port):
     # mitmdump -s ../proxy_add_headers.py -p RANDOM_PORT
-    logging.info("Launching ephemeral proxy server")
+    logging.debug("Launching ephemeral proxy server")
     process = subprocess.Popen(['mitmdump',
                                 '--script', 'mitmdump_script.py',
                                 '--listen-port', str(port)],
-                               preexec_fn=os.setsid,
-                               stderr=subprocess.STDOUT)
+                                #stdout=subprocess.DEVNULL,
+                                #stderr=subprocess.STDOUT,
+                                preexec_fn=os.setsid)
+
+
     return process
 
 
 def _kill_proxy_server(process):
 
-    process_ = psutil.Process(process.pid)
+    #process_ = psutil.Process(process.pid)
     # for proc in process_.children(recursive=True):
     #     proc.kill()
     # process_.kill()
     os.killpg(os.getpgid(process.pid), signal.SIGHUP)
     os.killpg(os.getpgid(process.pid), signal.SIGTERM)
 
-import time
 
 def main(args):
     nolog_args = _configure_logging(args)
@@ -74,14 +77,15 @@ def main(args):
     port = random.randint(1025, 65535)
     # port = 9192
     process = _run_proxy_server(port)
-    print(psutil.Process(process.pid).status())
+   # print(psutil.Process(process.pid).status())
     time.sleep(2)
-    print(psutil.Process(process.pid).status())
+    #print(psutil.Process(process.pid).status())
     logger.debug(openstack_args)
     _run_openstack(port, openstack_args)
-    print(psutil.Process(process.pid).status())
+   # print(psutil.Process(process.pid).status())
+   # time.sleep(2)
     _kill_proxy_server(process)
-    print(psutil.Process(process.pid).status())
+    #print(psutil.Process(process.pid).status())
 
 
 if __name__ == "__main__":
